18d6035aee7ae5399e9274c5af185fbf2d2530a8
Deylyson <deylyson.ti@gmail.com>
2020-03-26 02:24:57 -0300

    Adicionado arquivos PHP e CSS

---
+++ theweeknd.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>The Weeknd Bio</title>
+	<link rel="stylesheet" href="theweeknd.css">
+	  <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<script src="https://kit.fontawesome.com/aaac55f867.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+	<base href="http://example.com/" target="_blank, _self, _parent, _top">
+		<header id="header" class="">
+		<div id="div-header">
+			 <div class="header-top-row hidden-sm hidden-xs">
+			<div class="container">
+				<div class="top-welcome">
+					 <div id="account-menu" class="pull-left">
+					<p class="welcome-message"> 
+						<span> Bem-vindo XO, nós te amamos!</span>
+					</p>
+
+					<p>
+						<span>
+						<i class="fa fa-phone"></i>
+						(48) 0000-0000
+					</span>
+
+					<span>
+						<i class="fa fa-envelope"></i>
+						theweekndxo@gmail.com
+					</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+<img  class="banner"src="https://fbcoverstreet.com/thumbnail/o8TjOEKQKKLDjvbO4A8EEYi9mKByqWONZZiA9kawJTewYS2JeCAvqMsPYU6DDViw.webp" alt="">
+</div>
+	</header>
+	
+
+
+  <!-- Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>	
+</body>
+</html>
